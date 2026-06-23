ac897142e711acbb72423aebe82c330db6127610
cdevaleriano <cde.valeriano@gmail.com>
2026-06-23 19:45:44 +0800

    Testing

---
+++ src/data/ingest.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+def ingest_csv(path: str) -> pd.DataFrame:
+    """
+    Loads CSV data into a pandas DataFrame.
+
+    Args:
+        path (str): Path to the CSV file.
+
+    Returns:
+        pd.DataFrame: Loaded dataset.    
+    """
+
+    if not os.patha.exists(path):
+        raise FileNotFoundError(f"File not found: {path}")
+    
+    return pd.read_csv(path)
